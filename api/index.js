ab11f4f8f75f00817f12742b4b36a9f7e132124d
sunil-495 <sunilsinghshekhawat495@gmail.com>
2026-04-24 15:06:41 +0530

    updated ui

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:5173","http://localhost:8080", "https://restorent-management-eight.vercel.app", "https://restorent-management-g7de.vercel.app"],
+  origin: ["http://localhost:5173","https://restorent-management-eight.vercel.app"],
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
